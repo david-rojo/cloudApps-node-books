9f777bd0fdb4406b2e89928f2f17822f906c4d6a
david-rojo <davidrojotech@gmail.com>
2020-12-15 00:31:30 +0100

    user crud and postComment implemented

--- src/booksRouter.js
+++ src/booksRouter.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 
 const mongoUrl = "mongodb://localhost:27017/books";
 
+const Schema = mongoose.Schema;
+
 let Book;
+let User;
+let Comment;
 
 async function dbConnect() {
     
@@ -25,6 +29,33 @@ async function dbConnect() {
     });
 
     Book = mongoose.model('Book', bookSchema);
+
+    var userSchema = new mongoose.Schema({
+        nick: { 
+            type : String, 
+            unique : true
+        },
+        email: String
+    });
+
+    User = mongoose.model('User', userSchema);
+
+    var commentSchema = new mongoose.Schema({
+        text: String,
+        score: Number,
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        book: {
+            type: Schema.Types.ObjectId,
+            ref: 'Book',
+            required: true
+        }
+    });
+
+    Comment = mongoose.model('Comment', commentSchema);
 }
 
 async function init() {
@@ -46,6 +77,13 @@ async function init() {
 		publisher: "CloudApps Classics",
         publicationYear: 1943
     }).save();
+
+    await new User({
+        nick: "david",
+        email: "david@urjc.es"
+    }).save();
+
+    console.log("Data loading to Mongo completed");
 }
 
 const router = express.Router();
@@ -58,6 +96,18 @@ function validBook(book) {
         && typeof book.publicationYear == 'number';
 }
 
+function validUser(user) {
+    return typeof user.nick == 'string'
+        && typeof user.email == 'string';
+}
+
+function validComment(comment) {
+    return typeof comment.text == 'string'
+        && typeof comment.score == 'number'
+        && comment.score >=0 
+        && comment.score <=5;
+}
+
 function toResponse(document) {
 
     if (document instanceof Array) {
@@ -66,6 +116,7 @@ function toResponse(document) {
         let response = document.toObject({ versionKey: false });
         response.id = response._id.toString();
         delete response._id;
+        delete response.__v;
         return response;
     }
 }
@@ -80,9 +131,10 @@ function fullUrl(req) {
     return fullUrl + (fullUrl.endsWith('/') ? '' : '/');
 }
 
-router.post('/books', async (req, res) => {
+router.post('/nodebooks/api/v2/books', async (req, res) => {
 
     if (!validBook(req.body)) {
+        console.log("Invalid book request");
         res.sendStatus(400);
     } else {
 
@@ -101,20 +153,22 @@ router.post('/books', async (req, res) => {
     }
 });
 
-router.get('/books', async (req, res) => {
+router.get('/nodebooks/api/v2/books', async (req, res) => {
     const allBooks = await Book.find().exec();
     res.json(toResponse(allBooks));
 });
 
-router.get('/books/:id', async (req, res) => {
+router.get('/nodebooks/api/v2/books/:id', async (req, res) => {
     const id = req.params.id;
 
     if(!mongoose.Types.ObjectId.isValid(id)){
+        console.log("Invalid id");
         return res.sendStatus(400);
     }
 
     const book = await Book.findById(id);
     if (!book) {
+        console.log("book not found");
         res.sendStatus(404);
     } else {
         res.json(toResponse(book));
@@ -162,6 +216,115 @@ router.put('/books/:id', async (req, res) => {
 
             res.json(toResponse(book));
         }
+    }
+});
+
+router.post('/nodebooks/api/v2/books/:id/comments', async (req, res) => {
+    const id = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        console.log("Invalid id");
+        return res.sendStatus(400);
+    }
+
+    if (!validComment(req.body)) {
+        console.log("Invalid comment request");
+        res.sendStatus(400);
+    } else {
+
+        const book = await Book.findById(req.params.id).exec();
+        if (!book) {
+            console.log("book not found");
+            return res.sendStatus(404);
+        }
+        const user = await User.findOne({nick: req.body.nick}).exec();
+        if (!user) {
+            console.log("user not found");
+            return res.sendStatus(404);
+        }
+        const comment = await new Comment({
+            user,
+            text: req.body.text,
+            score: req.body.score,
+            book,
+        }).save();
+
+        return res.json(toResponse(comment, user));
+    }
+    
+});
+
+router.post('/nodebooks/api/v2/users', async (req, res) => {
+
+    if (!validUser(req.body)) {
+        console.log("invalid user request");
+        res.sendStatus(400);
+    } else {
+
+        const user = new User({
+            nick: req.body.nick,
+            email: req.body.email
+        });
+
+        await user.save();
+
+        res.location(fullUrl(req) + user.id);
+        res.json(toResponse(user));
+    }
+});
+
+router.get('/nodebooks/api/v2/users/:id', async (req, res) => {
+    const id = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        console.log("invalid id");
+        return res.sendStatus(400);
+    }
+
+    const user = await User.findById(id);
+    if (!user) {
+        console.log("user not found");
+        res.sendStatus(404);
+    } else {
+        res.json(toResponse(user));
+    }
+});
+
+router.patch('/nodebooks/api/v2/users/:id', async (req, res) => {
+    const id = req.params.id;
+    
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        console.log("invalid id");
+        return res.sendStatus(400);
+    }
+
+    const user = await User.findOneAndUpdate(
+        {id: req.params.id},
+        {email: req.body.email},
+        {new: true}
+    ).exec();
+    if (!user) {
+        console.log("user not found");
+        return res.sendStatus(404);
+    }
+    return res.json(toResponse(user));
+});
+
+router.delete('/nodebooks/api/v2/users/:id', async (req, res) => {
+    const id = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        console.log("invalid id");
+        return res.sendStatus(400);
+    }
+
+    const user = await User.findById(id);
+    if (!user) {
+        console.log("user not found");
+        res.sendStatus(404);
+    } else {
+        await User.findByIdAndDelete(id);
+        res.json(toResponse(user));
     }
 });
 
